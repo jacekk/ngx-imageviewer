202cb842ef429141ef986eab5b276f13e8160898
jacekk <github.com@jacek-k.net>
2018-04-07 23:04:38 +0200

    bumped alpha with some interfaces exported;

--- src/app/imageviewer/index.ts
+++ src/app/imageviewer/index.ts
@@ -1,3 +1,4 @@
-export { ImageViewerModule } from './imageviewer.module';
 export { ButtonConfig, ButtonStyle, createButtonConfig, ImageViewerConfig, IMAGEVIEWER_CONFIG } from './imageviewer.config';
 export { ImageViewerComponent } from './imageviewer.component';
+export { ImageViewerModule } from './imageviewer.module';
+export { ResourceLoadState, ResourceState, ResourceType } from './imageviewer.model';
